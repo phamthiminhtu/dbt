796fd08340790705242223c8b152f7f082c2bcec
tu.pham <you@example.com>
2023-10-15 17:52:36 +1100

    add comment

--- models/warehouse/facts_listings.sql
+++ models/warehouse/facts_listings.sql
@@ -1,10 +1,13 @@
 {#-
-	This model uses the "delete+insert" strategy using the run_date variable:
+	This model uses the "delete+insert" (i.e. insert overwrite) strategy
+	using the run_date variable:
 		It will delete data with scraped_date in the [run_date - 2,  run_date] range
 		and insert new data of that date range.
+
 	A few ways to define run_date variable:
 		1. Define the run_date in dbt_profiles.yml (default)
-		2. Overrided run_date in dbt_profiles.yml by passing vars using cli `dbt run --vars '{"run_date": "2023-10-01"}'`
+		2. Overrided run_date in dbt_profiles.yml by passing vars using
+			cli `dbt run --vars '{"run_date": "2023-10-01"}'`
 		3. Run with run_date=today by deleting the run_date in dbt_profiles.yml.
 -#}
 {%- set run_date = var('run_date', modules.datetime.datetime.today().strftime("%Y-%m-%d")) -%}
