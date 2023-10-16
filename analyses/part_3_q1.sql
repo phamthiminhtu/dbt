95357a3527dff1d858ff52bbd276fc9636667b67
tu.pham <you@example.com>
2023-10-17 10:48:42 +1100

    part_3_q1

---
+++ analyses/part_3_q1.sql
@@ -0,0 +1,25 @@
+WITH
+  facts_listings AS
+    (SELECT
+      *,
+      CASE WHEN has_availability = 't' THEN listing_id END AS active_listing_id,
+      CASE WHEN has_availability = 't' THEN price END AS active_listing_price,
+      30 - availability_30 AS number_of_stays
+    FROM "postgres"."warehouse"."facts_listings")
+
+  ,agg AS
+    (SELECT
+      listing_neighbourhood,
+      (
+        SUM(active_listing_price * number_of_stays)
+        / COUNT(DISTINCT active_listing_id)
+      ) AS estimated_revenue_per_active_listings
+    FROM facts_listings
+    GROUP BY
+      listing_neighbourhood)
+
+  SELECT
+    *,
+    ROW_NUMBER() OVER(ORDER BY estimated_revenue_per_active_listings DESC) AS ranking
+  FROM agg
+  ORDER BY ranking
